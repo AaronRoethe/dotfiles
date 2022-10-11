f892cc30a7bda62f3032cda869cd6d3078aa0ed8
Aaron Roethe <roethe.aaron@outlook.com>
2022-10-10 18:52:14 -0700

    add .gitconfig

--- .env/git.sh
+++
@@ -1,7 +0,0 @@
-alias glog="git log --graph --pretty='%Cred%h%Creset %Cgreen(%ad) %C(bold blue)%<(12,trunc)<%an> %C(auto)%d%Creset%<(80,trunc)%s' --date=short"
-alias ggpush="git push origin $(git_current_branch) -u"
-
-githome () { gcm && ggpull }
-
-mergedBranch () { gcm && ggpull && gfa && gbda }
-
