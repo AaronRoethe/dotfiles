ae0950226c50e0330f6db3c5e86c32f78df31e03
Aaron Roethe <roethe.aaron@outlook.com>
2023-02-25 13:47:40 -0700

    update pyC

--- .env/python.sh
+++ .env/python.sh
@@ -1,24 +1,32 @@
 alias pyA="source .venv/bin/activate" 
 alias pyR="cp ~/repos/utils/requirements.txt ."
 
-pyCreate () {
-	if [ -d .venv ];then 
-	    rm -rf .venv; fi &&
+pyC () {
+	location="requirements.txt"
+    venvName=".venv"
+    
+    # Parse flags
+    while getopts "r:e:" opt; do
+        case $opt in
+            e) venvName=$OPTARG ;;
+            r) location=$OPTARG ;;
+            \?) echo "Invalid option: -$OPTARG" ;;
+            :) echo "Option -$OPTARG requires an argument.";;
+        esac
+    done
+
+	# Remove existing virtual environment if it exists
+  	[ -d $venvName ] && rm -rf $venvName
 	
-	if [ -f .python-version ];then
-	    pyVersion="local"
-	else
-	    pyVersion="global"; fi &&
+	# Determine Python version (global or local)
+	pyVersion=$( [ -f .python-version ] && echo "local" || echo "global" )
 
-	location=${1:-requirements.txt} &&
-
-	if [ -f $location ];then
-	    action=(pip install -r $location)
-	else
-	    action=(echo "\n No file: $location \n"); fi &&	
-
-	PYENV_VERSION=`pyenv $pyVersion` python -m venv .venv &&
-	source .venv/bin/activate &&
-	pip install --upgrade pip && $action
+	# Check pyVersion, create version file, virtual environment, activate it
+	pyenv version-name $pyVersion | xargs -I {} pyenv local {} && python -m venv $venvName
+	source $venvName/bin/activate && pip install --upgrade pip
+	
+	# Install requirements if file exists, otherwise print error message
+	[ -f $location ] && pip install -r $location || echo -e "\nFile $location not found"
 }
+
 
