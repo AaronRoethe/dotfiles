301f101a47ec8b73e789ab5cc8adb075e2fd1526
Aaron Roethe <67488140+AaronRoethe@users.noreply.github.com>
2025-01-20 14:23:07 -0700

    Update remote_setup.sh

--- mac_bootstrap/remote_setup.sh
+++ mac_bootstrap/remote_setup.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-# Check if Homebrew is installed, if not install it
+# Check if Homebrew is installed, and install if necessary
 if ! command -v brew &> /dev/null; then
-  echo "Homebrew not found. Installing Homebrew..."
+  echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  
+  # Add Homebrew to PATH for new installations
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 else
   echo "Homebrew is already installed."
 fi
 
-# Add Homebrew to PATH (for new installations)
-if ! command -v brew &> /dev/null; then
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-fi
-
-# Install Git using Homebrew
+# Install Git using Homebrew if not installed
 if ! command -v git &> /dev/null; then
   echo "Installing Git..."
   brew install git
@@ -22,11 +20,13 @@ else
   echo "Git is already installed."
 fi
 
-# Print success message
-echo "Git is installed. You can now clone your dotfile repository."
-
+# Clone dotfiles repository as a bare repo
 git clone --bare https://github.com/AaronRoethe/dotfiles.git $HOME/.cfg
 
+# Create an alias for managing dotfiles
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
+# Set Git to not show untracked files for the dotfiles
 config config --local status.showUntrackedFiles no
+
+echo "Dotfiles repository cloned and setup."
